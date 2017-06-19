9e667829127d2f42dfc8ba32813ef403100d0fac
ddorn <diego.dorn@free.fr>
2017-06-19 20:10:37 +0200

    Windows class (empty project structure)

--- GUI/vracabulous.py
+++ GUI/vracabulous.py
@@ -1,7 +1,7 @@
 # coding=utf-8
-
+import os
 import pygame
-
+from pygame.locals import *
 from collections import defaultdict
 
 from GUI.locals import FLASH_CREEN, MIDNIGHT_BLUE
@@ -175,4 +175,55 @@ class Separator:
         return self * (1 / other)
 
 
-__all__ = ['FocusSelector', 'FPSIndicator', 'Separator']
+class Windows:
+
+    SCREEN_SIZE = 800, 500
+    NAME = 'Empty project'
+
+    def __init__(self):
+        self.running = True
+        self.screen = self.new_screen()
+        self.clock = pygame.time.Clock()
+
+        self.fps = FPSIndicator(self.clock)
+
+    def update_on_event(self, e):
+        if e.type == QUIT:
+            self.running = False
+
+        elif e.type == KEYDOWN:
+            if e.key == K_ESCAPE:
+                self.running = False
+
+            elif e.key == K_F4 and e.mod & KMOD_ALT:  # Alt+F4 --> quits
+                self.running = False
+
+        elif e.type == VIDEORESIZE:
+            self.SCREEN_SIZE = e.size
+            self.screen = self.new_screen()
+
+    def update(self):
+        for e in pygame.event.get():
+            self.update_on_event(e)
+
+
+    def render(self):
+        raise NotImplementedError
+
+    def destroy(self):
+        pass
+
+    def run(self):
+        while self.running:
+            self.update()
+            self.render()
+        self.destroy()
+
+    def new_screen(self):
+        os.environ['SDL_VIDEO_CENTERED'] = '1'
+        pygame.display.set_caption(self.NAME)
+
+        return pygame.display.set_mode(self.SCREEN_SIZE, DOUBLEBUF | VIDEORESIZE)
+
+
+__all__ = ['FocusSelector', 'FPSIndicator', 'Separator', 'Windows']
